7b02a8849687f4cb4f47d933940ec348067c673e
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-01-06 18:09:36 +0300

    Time: 60 ms (73.98%) | Memory: 45.2 MB (49.42%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -10,21 +10,18 @@ var candy = function(ratings) {
 
   let candies = Array(ratings.length).fill(1);
 
-  // Left to right pass
   for (let i = 1; i < ratings.length; i++) {
     if (ratings[i] > ratings[i - 1]) {
       candies[i] = candies[i - 1] + 1;
     }
   }
 
-  // Right to left pass
   for (let i = ratings.length - 2; i >= 0; i--) {
     if (ratings[i] > ratings[i + 1]) {
       candies[i] = Math.max(candies[i], candies[i + 1] + 1);
     }
   }
 
-  // Sum up the candies array to get the total
   let totalCandies = candies.reduce((sum, val) => sum + val, 0);
 
   return totalCandies;
